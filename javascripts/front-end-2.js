2a1ad31797a9ba0beabed68d0f36c78b2d0c4275
ericdodds <eric@theironyard.com>
2013-10-05 14:57:00 -0400

    Site updated at 2013-10-05 18:57:00 UTC

---
+++ javascripts/front-end-2.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+  
+  // FAQ open/close behavior
+  //------------------------------------------------------------------------------------------------//
+  
+  $('li.question.open').removeClass('open');
+
+  var $questions = $('.faq-questions li.question');
+  
+  $questions.on('click', 'h3', function(){
+    $questions.each(function() {
+      $(this).removeClass('open');
+    });
+
+    $(this).parents('.question').addClass('open');
+  });
+
+
+  // Curriculum overview open/close behavior
+  //------------------------------------------------------------------------------------------------//
+
+  var $courses = $('.curriculum-overview .columns>ul li');
+
+  $courses.on('click', function() {
+
+    var $this = $(this);
+    
+    $courses.each(function() {
+      $(this).removeClass('open');
+    });
+
+    if ($this.hasClass('open')) {
+      $this.removeClass('open');
+    } else {
+      $this.addClass('open');
+    }
+  });
+
+
+  // Flowtype.js
+  // http://simplefocus.com/flowtype
+  //------------------------------------------------------------------------------------------------//
+
+  $('.fed-2 h2').fitText(1.15, {maxFontSize: '35px'});
+
+
+  // Sequence.js slider
+  //------------------------------------------------------------------------------------------------//
+
+  var sequence = $(".slider").sequence({
+    autoPlay: true,
+    autoPlayDelay: 3000,
+    moveActiveFrameToTop: true,
+    navigationSkipThreshold: 1000,
+    fadeFrameWhenSkipped: false,
+    prevButton: $('.prev'),
+    nextButton: $('.next'),
+    animateStartingFrameIn: false
+  }).data("sequence");
+
+});
